96ad4c22224858003000ef0a7f0a8735f4975d77
Matheus Sousa <ma.sousa.soares@gmail.com>
2026-05-22 23:17:42 -0300

    feat: init AdicionarSubscription test

--- PaymentContext.Tests/StudantTests.cs
+++ PaymentContext.Tests/StudantTests.cs
@@ -6,8 +6,10 @@ namespace PaymentContext.Tests;
 public sealed class StudantTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void AdicionarSubscription()
     {
-        
+        var subscription = new Subscription(null);
+        var studant = new Studant("Matheus", "Sousa", "matheus@gmail.com", "12345678910", "Rua 01");
+        studant.AddSubscription(subscription);  
     }
 }
