9776ce47a8d21ff638d1b3547d441b4a8469a048
Matheus Sousa <ma.sousa.soares@gmail.com>
2026-06-23 00:06:17 -0300

    test: add StudentTests and DocumentTest

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,10 +1,47 @@
-﻿namespace PaymentContext.Tests.Entities;
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Entities;
 
 [TestClass]
-public class StudentTests
+public sealed class StudentTests
 {
+    private static Name NewName() => new Name("Matheus", "Sousa");
+    private static Document NewDocument() => new Document("12345678910", Domain.Enuns.EDocumentType.CPF);
+    private static Email NewEmail() => new Email("matheus@gmail.com");
+    private static Address NewAddress() =>
+        new Address("Rua 01", "100", "Jd. Europa", "Sorocaba", "SP", "Brasil", "99999999");
+    private static Subscription NewSubscription() => new Subscription(null, NewAddress());
+    
+    
     [TestMethod]
-    public void TestMethod1()
+    public void AddSubscription_ShouldReturnSuccess()
     {
+        var name = NewName();
+        var document = NewDocument();
+        var email = NewEmail();
+        var address = NewAddress();
+        var subscription = NewSubscription();
+
+        var student = new Student(name, email, document, address);
+        student.AddSubscription(subscription);
+        
+        Assert.IsTrue(student.IsValid);
+    }
+    
+    [TestMethod]
+    public void AddTwoSubscriptions_ShouldReturnError()
+    {
+        var name = NewName();
+        var document = NewDocument();
+        var email = NewEmail();
+        var address = NewAddress();
+        var subscription = NewSubscription();
+
+        var student = new Student(name, email, document, address);
+        student.AddSubscription(subscription);
+        student.AddSubscription(subscription);
+        
+        Assert.IsFalse(student.IsValid);
     }
 }
---
+++ PaymentContext.Tests/ValueObjects/DocumentTest.cs
@@ -0,0 +1,23 @@
+﻿namespace PaymentContext.Tests.ValueObjects;
+
+[TestClass]
+public class DocumentTest
+{
+    [TestMethod]
+    [DataRow("")]
+    [DataRow("123")]
+    [DataRow("ABC")]
+    public void CNPJInvalid_ShouldReturnError(string cnpj)
+    {
+        
+    }
+    
+    [TestMethod]
+    [DataRow("91991312000160")]
+    [DataRow("97727294000190")]
+    [DataRow("31001221000108")]
+    public void CNPJValid_ShouldReturnSuccess(string cnpj)
+    {
+        
+    }
+}
