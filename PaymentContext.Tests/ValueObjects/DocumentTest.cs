98c02b0682748bf15be2d9c27f0977a0be93c935
SousaMatheus <ma.sousa.soares@gmail.com>
2026-06-25 09:53:04 -0300

    test: `Document`

--- PaymentContext.Tests/ValueObjects/DocumentTest.cs
+++ PaymentContext.Tests/ValueObjects/DocumentTest.cs
@@ -1,4 +1,6 @@
-﻿namespace PaymentContext.Tests.ValueObjects;
+﻿using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.ValueObjects;
 
 [TestClass]
 public class DocumentTest
@@ -9,15 +11,19 @@ public class DocumentTest
     [DataRow("ABC")]
     public void CNPJInvalid_ShouldReturnError(string cnpj)
     {
-        
+        var document = new Document(cnpj, Domain.Enuns.EDocumentType.CNPJ);
+
+        Assert.IsFalse(document.IsValid);
     }
-    
+
     [TestMethod]
     [DataRow("91991312000160")]
     [DataRow("97727294000190")]
     [DataRow("31001221000108")]
     public void CNPJValid_ShouldReturnSuccess(string cnpj)
     {
-        
+        var document = new Document(cnpj, Domain.Enuns.EDocumentType.CNPJ);
+
+        Assert.IsTrue(document.IsValid);
     }
 }
